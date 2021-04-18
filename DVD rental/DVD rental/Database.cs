405e1274e17ccd4d7bbce1fc637ed593d1ea85fe
Abdock <79397710+Abdock@users.noreply.github.com>
2021-04-18 20:57:53 +0600

    Add files via upload

---
+++ DVD rental/DVD rental/Database.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using Npgsql;
+
+namespace DVD_rental
+{
+    public class Database
+    {
+        private readonly string _path = "Host=localhost;Port=5500;Username=postgres;Password=allocator123;Database=dvdrental";
+        private NpgsqlConnection _connection;
+
+        public Database()
+        {
+            Init();
+        }
+
+        public Database(string port, string username, string password, string database)
+        {
+            _path = $"Host=localhost;Port={port};Username={username};Password={password};Database={database}";
+            Init();
+        }
+
+        private void Init()
+        {
+            _connection = new NpgsqlConnection(_path);
+            _connection.Open();
+        }
+
+        public List<string> GetFilms()
+        {
+            var res = new List<string>();
+            var query = "SELECT film.title, film.release_year, language.name, film.length, film.rating FROM film JOIN language ON film.language_id = language.language_id;";
+            var cmd = new NpgsqlCommand(query, _connection);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                res.Add((string)reader[0]);
+            }
+            reader.Dispose();
+            return res;
+        }
+
+        public List<string> GetLanguages()
+        {
+            var result = new List<string>();
+            var query = "SELECT name FROM language";
+            var cmd = new NpgsqlCommand(query, _connection);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                result.Add((string)reader[0]);
+            }
+            reader.Dispose();
+            return result;
+        }
+
+        public List<string> GetCategories()
+        {
+            var query = "SELECT name FROM category";
+            var res = new List<string>();
+            var cmd = new NpgsqlCommand(query, _connection);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                res.Add((string) reader[0]);
+            }
+            reader.Dispose();
+            return res;
+        }
+
+        public List<string> GetRatings()
+        {
+            var query = "SELECT rating FROM film GROUP BY rating";
+            var res = new List<string>();
+            var cmd = new NpgsqlCommand(query, _connection);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                res.Add((string)reader[0]);
+            }
+            reader.Dispose();
+            return res;
+        }
+
+        public List<string> GetFilmsWithActor(string name, string surname)
+        {
+            var query =
+                $"SELECT film.title, film.length, film.rating, language.name, category.name FROM film JOIN film_category ON film.film_id = film_category.film_id JOIN category ON film_category.category_id = category.category_id JOIN language ON film.language_id = language.language_id JOIN film_actor ON film.film_id = film_actor.film_id JOIN actor ON film_actor.actor_id = actor.actor_id WHERE actor.first_name = '{name}' AND actor.last_name = '{surname}';";
+            var cmd = new NpgsqlCommand(query, _connection);
+            var reader = cmd.ExecuteReader();
+            var res = new List<string>();
+            while (reader.Read())
+            {
+                res.Add( reader[0] + "\t" + reader[1] + "\t" + reader[2] + "\t" + reader[3] + "\t" + reader[4]);
+            }
+            reader.Dispose();
+            return res;
+        }
+
+        public List<string> GetFilmsWithFilter(string filterQuery)
+        {
+            var query =
+                "SELECT film.title, film.length, film.rating, language.name, category.name FROM film JOIN film_category ON film.film_id = film_category.film_id JOIN category ON film_category.category_id = category.category_id JOIN language ON film.language_id = language.language_id";
+            if (filterQuery.Length > 0)
+            {
+                query += " WHERE " + filterQuery;
+            }
+            var cmd = new NpgsqlCommand(query, _connection);
+            var reader = cmd.ExecuteReader();
+            var res = new List<string>();
+            while (reader.Read())
+            {
+                res.Add(reader[0] + "\t" + reader[1] + "\t" + reader[2] + "\t" + reader[3] + "\t" + reader[4]);
+            }
+            reader.Dispose();
+            return res;
+        }
+
+        public List<string> GetFilmsWithCost(string title)
+        {
+            var query = $"SELECT film.title, film.length, film.rating, language.name, category.name FROM film JOIN film_category ON film.film_id = film_category.film_id JOIN category ON film_category.category_id = category.category_id JOIN language ON film.language_id = language.language_id, (SELECT film.replacement_cost AS cost FROM film WHERE film.title = \'{title}\') AS tb WHERE tb.cost = film.replacement_cost;";
+            var res = new List<string>();
+            var cmd = new NpgsqlCommand(query, _connection);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                res.Add(reader[0] + "\t" + reader[1] + "\t" + reader[2] + "\t" + reader[3] + "\t" + reader[4]);
+            }
+            reader.Dispose();
+            return res;
+        }
+
+        public List<string> GetActorsWithFilter(string filterQuery)
+        {
+            var query = "SELECT actor.first_name, actor.last_name FROM actor";
+            if (filterQuery.Length > 0)
+            {
+                query += " WHERE " + filterQuery;
+            }
+            var res = new List<string>();
+            var cmd = new NpgsqlCommand(query, _connection);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                res.Add(reader[0] + "\t" + reader[1]);
+            }
+            reader.Dispose();
+            return res;
+        }
+    }
+}
---
+++ DVD rental/DVD rental/MainWindow.xaml.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DVD_rental
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Database _database = new Database();
+        private QueryType _query;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ResizeRender(object sender, SizeChangedEventArgs args)
+        {
+            //Languages filter grid
+            LanguagesList.Width = args.NewSize.Width * 0.25;
+            //Film filter grid
+            FilmTitleInput.Width = args.NewSize.Width * 0.2;
+            LanguageSelect.Width = args.NewSize.Width * 0.2;
+            FilmLengthFrom.Width = args.NewSize.Width * 0.1;
+            FilmLengthTo.Width = args.NewSize.Width * 0.1;
+            RatingSelect.Width = args.NewSize.Width * 0.2;
+            CategorySelect.Width = args.NewSize.Width * 0.2;
+            //Actor filter grid
+            ActorFirstNameInput.Width = args.NewSize.Width * 0.2;
+            ActorLastNameInput.Width = args.NewSize.Width * 0.2;
+        }
+
+        private void HideFilters()
+        {
+            FilmsFilter.Visibility = Visibility.Hidden;
+            LanguagesFilter.Visibility = Visibility.Hidden;
+            ActorsFilter.Visibility = Visibility.Hidden;
+        }
+
+        private void InputFilmLength(object sender, TextCompositionEventArgs args)
+        {
+            args.Handled = new Regex("[^0-9]").IsMatch(args.Text);
+        }
+        
+        private void SelectLanguagesClick(object sender, RoutedEventArgs args)
+        {
+            HideFilters();
+            _query = QueryType.Language;
+            LanguagesFilter.Visibility = Visibility.Visible;
+            LanguagesList.Items.Clear();
+            var languages = _database.GetLanguages();
+            foreach (var item in languages)
+            {
+                LanguagesList.Items.Add(new TextBlock() {Text = item});
+            }
+        }
+        
+        private void SelectFilmsClick(object sender, RoutedEventArgs args)
+        {
+            HideFilters();
+            _query = QueryType.Film;
+            FilmsFilter.Visibility = Visibility.Visible;
+            var languages = _database.GetLanguages();
+            var categories = _database.GetCategories();
+            var ratings = _database.GetRatings();
+            LanguageSelect.Items.Clear();
+            foreach (var item in languages)
+            {
+                LanguageSelect.Items.Add(new TextBlock() {Text = item});
+            }
+            CategorySelect.Items.Clear();
+            foreach (var item in categories)
+            {
+                CategorySelect.Items.Add(new TextBlock() {Text = item});
+            }
+            RatingSelect.Items.Clear();
+            foreach (var item in ratings)
+            {
+                RatingSelect.Items.Add(new TextBlock() {Text = item});
+            }
+        }
+
+        private void SelectActorsClick(object sender, RoutedEventArgs args)
+        {
+            HideFilters();
+            _query = QueryType.Actor;
+            ActorsFilter.Visibility = Visibility.Visible;
+        }
+
+        private void SelectItemFromOutput(object sender, RoutedEventArgs args)
+        {
+            if (_query == QueryType.Film)
+            {
+                var from = (ListBoxItem) sender;
+                var title = from.Content.ToString().Split('\t')[0];
+                var sameFilms = _database.GetFilmsWithCost(title);
+                Info.Items.Clear();
+                foreach (var film in sameFilms)
+                {
+                    var item = new ListBoxItem() {Content = film};
+                    Info.Items.Add(item);
+                }
+            }
+            else if (_query == QueryType.Actor)
+            {
+                var from = (ListBoxItem) sender;
+                var data = from.Content.ToString().Split('\t');
+                var films = _database.GetFilmsWithActor(data[0], data[1]);
+                Info.Items.Clear();
+                foreach (var film in films)
+                {
+                    var item = new ListBoxItem() {Content = film};
+                    Info.Items.Add(item);
+                }
+            }
+            else if (_query == QueryType.Language)
+            {
+                var from = (ListBoxItem) sender;
+                var language = from.Content.ToString().Split("\t")[3];
+                var query = $"language.name = '{language}'";
+                var films = _database.GetFilmsWithFilter(query);
+                Info.Items.Clear();
+                foreach (var film in films)
+                {
+                    var item = new ListBoxItem() {Content = film};
+                    Info.Items.Add(item);
+                }
+            }
+        }
+        
+        private void AddQuery(object sender, RoutedEventArgs args)
+        {
+            if (_query == QueryType.Film)
+            {
+                var title = FilmTitleInput.Text;
+                var language = LanguageSelect.SelectedIndex;
+                if (!int.TryParse(FilmLengthFrom.Text, out var lenFrom))
+                {
+                    lenFrom = 0;
+                }
+                if (!int.TryParse(FilmLengthTo.Text, out var lenTo))
+                {
+                    lenTo = -1;
+                }
+                var category = CategorySelect.SelectedIndex;
+                var rating = RatingSelect.SelectedIndex;
+                var query = "";
+                if (title.Length > 0)
+                {
+                    query += $"film.title LIKE '%{title}%'";
+                }
+                if (lenTo != -1)
+                {
+                    if (query.Length > 0)
+                    {
+                        query += " AND ";
+                    }
+                    query += $"film.length BETWEEN {lenFrom} AND {lenTo}";
+                }
+                if (language != -1)
+                {
+                    if (query.Length > 0)
+                    {
+                        query += " AND ";
+                    }
+                    query += $"film.language_id = {language + 1}";
+                }
+                if (rating != -1)
+                {
+                    if (query.Length > 0)
+                    {
+                        query += " AND ";
+                    }
+                    query += $"film.rating = \'{((TextBlock) RatingSelect.SelectedItem).Text}\'";
+                }
+                if (category != -1)
+                {
+                    if (query.Length > 0)
+                    {
+                        query += " AND ";
+                    }
+                    query += $"category.category_id = {category + 1}";
+                }
+                var films = _database.GetFilmsWithFilter(query);
+                Output.Items.Clear();
+                foreach (var film in films)
+                {
+                    var item = new ListBoxItem {Content = film};
+                    item.Selected += SelectItemFromOutput;
+                    Output.Items.Add(item);
+                }
+            }
+            else if (_query == QueryType.Actor)
+            {
+                var name = ActorFirstNameInput.Text;
+                var surname = ActorLastNameInput.Text;
+                var query = "";
+                if (name.Length > 0)
+                {
+                    query += $"actor.first_name LIKE '%{name}%'";
+                }
+
+                if (surname.Length > 0)
+                {
+                    if (query.Length > 0)
+                    {
+                        query += " AND ";
+                    }
+                    query += $"actor.last_name LIKE '%{surname}%'";
+                }
+                
+                var actors = _database.GetActorsWithFilter(query);
+                Output.Items.Clear();
+                foreach (var actor in actors)
+                {
+                    var item = new ListBoxItem() {Content = actor};
+                    item.Selected += SelectItemFromOutput;
+                    Output.Items.Add(item);
+                }
+            }
+            else if (_query == QueryType.Language)
+            {
+                int index = LanguagesList.SelectedIndex;
+                var query = "";
+                if (index != -1)
+                {
+                    query += $"film.language_id = {index + 1}";
+                }
+                var films = _database.GetFilmsWithFilter(query);
+                Output.Items.Clear();
+                foreach (var film in films)
+                {
+                    var item = new ListBoxItem() {Content = film};
+                    item.Selected += SelectItemFromOutput;
+                    Output.Items.Add(item);
+                }
+            }
+        }
+    }
+}
